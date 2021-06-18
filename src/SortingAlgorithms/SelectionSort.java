f3f9568937f035587b5ef837fb4c6d7b4eee8b1f
Stefan Vlajkovic <vlajkovicstefan01@gmail.com>
2021-06-18 20:18:13 +0200

    add selectionsort

---
+++ src/SortingAlgorithms/SelectionSort.java
@@ -0,0 +1,27 @@
+package SortingAlgorithms;
+
+public class SelectionSort<T extends Comparable<T>> extends SortingAlgorithm<T> {
+    @Override
+    public void sort(T[] array) {
+        int size = array.length;
+
+        for(int step = 0; step < size - 1; step++){
+            int min_idx = step;
+
+            for(int i = step + 1; i < size; i++){
+                if (array[i].compareTo(array[min_idx]) < 0){
+                    min_idx = i;
+                }
+            }
+
+            T temp = array[step];
+            array[step] = array[min_idx];
+            array[min_idx] = temp;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Selection sort";
+    }
+}
