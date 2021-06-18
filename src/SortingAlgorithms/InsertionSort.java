763a40699033e2d0d9df0c0b39c64ad5da52f8cc
Stefan Vlajkovic <vlajkovicstefan01@gmail.com>
2021-06-18 20:13:58 +0200

    add insertion sort

---
+++ src/SortingAlgorithms/InsertionSort.java
@@ -0,0 +1,25 @@
+package SortingAlgorithms;
+
+public class InsertionSort<T extends Comparable<T>> extends SortingAlgorithm<T>{
+    @Override
+    public void sort(T[] array) {
+        int size = array.length;
+
+        for (int step = 1; step < size; step++){
+            T key = array[step];
+            int j = step - 1;
+
+            while (j >= 0 && key.compareTo(array[j]) < 0){
+                array[j+1] = array[j];
+                --j;
+            }
+
+            array[j+1] = key;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Insertion sort";
+    }
+}
